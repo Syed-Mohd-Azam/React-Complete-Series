33758de3a7062d425c259cd64d7934fb6a9e6160
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-11-07 22:50:02 +0530

    Components Composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/public/vite.svg";
 import "./App.css";
-
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 function App() {
-  const handleNameChange = () => {
-    const names = ["Syed", "Mohd", "Azam"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1> React Complete Series</h1>
-      <h1>Hello {handleNameChange()}</h1>
+      <Header />
+      <Content />
+      <Footer />
     </>
   );
 }
