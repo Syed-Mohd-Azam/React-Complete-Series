d598a63dbe4976fbfc1354a9a6311c8f4e25b02c
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-11-09 23:26:20 +0530

    Styling of third button done

--- src/Content.jsx
+++ src/Content.jsx
@@ -58,7 +58,18 @@ const Content = () => {
         >
           Count Decrease
         </button>
-        <button onClick={(e) => handleClickThree(e)}>Button Three</button>
+        <button
+          onClick={(e) => handleClickThree(e)}
+          style={{
+            padding: "5px",
+            color: "brown",
+            border: "1px solid black",
+            borderRadius: "5px",
+            backgroundColor: "white",
+          }}
+        >
+          Button Three
+        </button>
       </main>
     </>
   );
