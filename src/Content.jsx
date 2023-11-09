c77743ec391b8614cc5f678e7fcfa1452b9f0408
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-11-09 22:17:18 +0530

    useState( ) hook used

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,26 +1,24 @@
+import { useState } from "react";
 const Content = () => {
+  const [name, setName] = useState("Syed");
   const handleNameChange = () => {
     const names = ["Syed", "Mohd", "Azam"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
-  const handleClickOne = () => {
-    console.log("You clicked it!");
-  };
+
   const handleClickTwo = (name) => {
     console.log(`${name} Clicked!`);
   };
   const handleClickThree = (event) => {
-    console.log(event.target);
+    console.log(event.target.innerText);
   };
   return (
     <>
       <main>
-        <p>Hello {handleNameChange()}!</p>
-        <button onClick={handleClickOne}>Button One</button>
-        <button onClick={() => handleClickTwo(handleNameChange())}>
-          Button Two
-        </button>
+        <p>Hello {name}!</p>
+        <button onClick={handleNameChange}>Button One</button>
+        <button onClick={() => handleClickTwo(name)}>Button Two</button>
         <button onClick={(e) => handleClickThree(e)}>Button Three</button>
       </main>
     </>
